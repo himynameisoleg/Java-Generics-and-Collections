19dbac85cc0f9ac5b06be54f2941d30d91c0f0d6
himynameisoleg <perchykoleg@gmail.com>
2021-03-02 21:29:50 -0600

    Collections task 2 completed.

--- Collections/task2/src/Counter.java
+++ Collections/task2/src/Counter.java
@@ -2,20 +2,23 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class Counter<E> {
-    private HashMap/* TODO 
-The HashMap should accomodate a generic class for the keys and Integer for the value*/ counts;
+    private HashMap <E, Integer> counts = new HashMap<>();
 
-    public Counter(E[] things){
-/* TODO
-Loop through the elements of "things" and use the HashMap "counts"
-to store the elements of things as keys, and the number of times they
-appear in the array as values. Check the instructions. */
+    public Counter(E[] things) {
+
+        for (int i = 0; i < things.length; i++) {
+            Integer count = 1;
+
+            for (E c: counts.keySet()) {
+                if (c.equals(things[i])) count++;
+            }
+
+            counts.put(things[i], count);
+        }
     }
 
     public Integer getCount(E element){
-        /* TODO 
-This just returns the specific count for an element. It is a wrapper
-for a getting the value of the HashMap given the key "element" */
+        return counts.get(element);
     }
 
     public int getSize(){
@@ -23,7 +26,16 @@ for a getting the value of the HashMap given the key "element" */
     }
 
     public E mostFrequent(){
-/* TODO 
-Returns the most frequent key in the HashMap*/
+        Integer max = 0;
+        E maxKey = null;
+
+        for (E key : counts.keySet() ) {
+            if (counts.get(key) > max) {
+                max = counts.get(key);
+                maxKey = key;
+            }
+        }
+
+        return maxKey;
     }
 }
